1280f08d06a0c9c2818465c9e81e824b8f08b741
Dave Collie <dave.collie@vidyard.com>
2017-06-18 19:57:28 -0400

    Make directory and move unrared file into place

--- move_torrent.sh
+++ move_torrent.sh
@@ -6,17 +6,18 @@ temp_folder=""
 find /$TR_TORRENT_DIR/$TR_TORRENT_NAME -name "*.rar" -execdir nice -n 15 unrar e ${temp_folder} -o- "{}" \;
 
 if [ $? -ne 0]; then
-	echo "No torrent to unrar or failed to unpack rar."
-	exit 0
+  echo "No torrent to unrar or failed to unpack rar."
+  exit $?
 fi
 
 # check complete folder for files and place them in the correct folders
 for filename in $temp_folder; do
-	rel_path="$(./guess_path.py ${filename})"
+  echo "Checking ${filename}"
+  rel_path="$(./guess_path.py ${filename})"
 
-	if [ $? -eq 0]; then
-		mkdir -p ${base_folder}/${rel_path}
-		mv filename ${base_folder}/${rel_path}
-	fi
+  if [ $? -eq 0]; then
+    echo "Moving ${filename} to ${base_folder}/${rel_path}"
+    mkdir -p ${base_folder}/${rel_path} && mv filename ${base_folder}/${rel_path}
+  fi
 done
 
