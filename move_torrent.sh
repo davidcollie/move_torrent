b1fd143b6b306b7bce1f596eae7891b262bcb05e
Dave Collie <dave.collie@vidyard.com>
2017-07-11 14:27:03 -0400

    Fix path issue

--- move_torrent.sh
+++ move_torrent.sh
@@ -1,21 +1,24 @@
 #!/bin/bash
 # Call script from transmission when torrent has completed
+export PATH="$PATH:/usr/local/bin"
+
 base_folder=""
 complete_folder=""
 temp_folder=""
+torrent="$TR_TORRENT_DIR/$TR_TORRENT_NAME";
 
-echo "Checking for rar in ${TR_TORRENT_DIR}"
-find $TR_TORRENT_DIR -name "*.rar" -exec unrar e "{}" ${complete_folder} \;
+echo "Checking for rar in $torrent"
+find $torrent -name "*.rar" -exec unrar e "{}" ${complete_folder} \;
 
 if [ $? -ne 0]; then
-  echo "No torrent to unrar or failed to unpack rar."
+  echo "Failed to unpack rar."
   exit $?
 fi
 
 # check complete folder for files and place them in the correct folders
 for filename in $temp_folder; do
   echo "Checking ${filename}"
-  rel_path=$(./guess_path.py "${filename}")
+  rel_path=$(guess_path.py "${filename}")
 
   if [ $? -eq 0]; then
     echo "Moving ${filename} to ${base_folder}/${rel_path}"
